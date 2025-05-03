1a11ce5e662692b57f9c8e74e171ed88c9142034
Youssef Khaled Abdou <yossefkhaled551@gmail.com>
2025-05-03 03:13:18 +0300

    Add Category Service

---
+++ TechXpress.Services/CategoriesService/CategoryService.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using TechXpress.Data.Models;
+using TechXpress.Data.Repositories.CategoryRepo;
+using TechXpress.Data.UnitOfWork;
+using TechXpress.Services.GenericServices;
+
+namespace TechXpress.Services.CategoriesService
+{
+    public class CategoryService : GenericService<Category>, ICategoryService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly ICategoryRepository _categoryRepository;
+
+        public CategoryService(IUnitOfWork unitOfWork)
+            : base(unitOfWork.Categories)
+        {
+            _unitOfWork = unitOfWork;
+            _categoryRepository = unitOfWork.Categories as ICategoryRepository;
+        }
+
+        public async Task<string> Test()
+        {
+            var categories = await _categoryRepository.GetAllWithProductsAsync();
+            return $"{categories.Count()} categories loaded | Category Service is operational";
+        }
+
+        public async Task<IEnumerable<Category>> GetAllWithProductsAsync()
+        {
+            return await _categoryRepository.GetAllWithProductsAsync();
+        }
+
+        public async Task<Category> GetByIdWithProductsAsync(int id)
+        {
+            return await _categoryRepository.GetByIdWithProductsAsync(id);
+        }
+
+        public async Task<int> GetProductCountAsync(int categoryId)
+        {
+            return await _categoryRepository.GetProductCountAsync(categoryId);
+        }
+
+        public async Task<Dictionary<int, int>> GetProductCountForAllCategoriesAsync()
+        {
+            return await _categoryRepository.GetProductCountForAllCategoriesAsync();
+        }
+
+        public async Task<IEnumerable<Category>> GetFilteredCategoriesAsync(
+            Expression<Func<Category, bool>> filter = null,
+            Func<IQueryable<Category>, IOrderedQueryable<Category>> orderBy = null,
+            string includeProperties = "",
+            int? skip = null,
+            int? take = null)
+        {
+            return await _categoryRepository.GetFilteredCategoriesAsync(
+                filter,
+                orderBy,
+                includeProperties,
+                skip,
+                take);
+        }
+
+        public async Task<IEnumerable<Category>> SearchCategoriesAsync(string searchTerm)
+        {
+            return await _categoryRepository.SearchCategoriesAsync(searchTerm);
+        }
+    }
+}
---
+++ TechXpress.Services/CategoriesService/ICategoryService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using TechXpress.Data.Models;
+using TechXpress.Services.GenericServices;
+
+namespace TechXpress.Services.CategoriesService
+{
+    public interface ICategoryService : IGenericService<Category>
+    {
+        Task<string> Test();
+
+        // Category-specific operations
+        Task<IEnumerable<Category>> GetAllWithProductsAsync();
+        Task<Category> GetByIdWithProductsAsync(int id);
+        Task<int> GetProductCountAsync(int categoryId);
+        Task<Dictionary<int, int>> GetProductCountForAllCategoriesAsync();
+
+        // Advanced filtering
+        Task<IEnumerable<Category>> GetFilteredCategoriesAsync(
+            Expression<Func<Category, bool>> filter = null,
+            Func<IQueryable<Category>, IOrderedQueryable<Category>> orderBy = null,
+            string includeProperties = "",
+            int? skip = null,
+            int? take = null);
+
+        Task<IEnumerable<Category>> SearchCategoriesAsync(string searchTerm);
+    }
+}
