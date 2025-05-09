ed0f7e5534f153a2f7bea94e38c44fb2570e29a6
Youssef Khaled Abdou <yossefkhaled551@gmail.com>
2025-05-09 18:03:36 +0300

    Enhance Order Filteration

---
+++ TechXpress.Web/Extensions/EnumExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Reflection;
+
+namespace TechXpress.Web.Extensions
+{
+    public static class EnumExtensions
+    {
+        public static string GetDisplayName(this Enum enumValue)
+        {
+            var member = enumValue.GetType().GetMember(enumValue.ToString()).FirstOrDefault();
+            if (member != null)
+            {
+                var display = member.GetCustomAttribute<DisplayAttribute>();
+                return display?.Name ?? enumValue.ToString();
+            }
+            return enumValue.ToString();
+        }
+
+        // For the whole enum type: get list of (Value, Display Name)
+        public static IEnumerable<KeyValuePair<string, string>> GetEnumSelectList(this Type enumType)
+        {
+            if (!enumType.IsEnum)
+                throw new ArgumentException("Type must be an enum");
+
+            return Enum.GetValues(enumType)
+                .Cast<Enum>()
+                .Select(e => new KeyValuePair<string, string>(e.ToString(), e.GetDisplayName()));
+        }
+    }
+}
